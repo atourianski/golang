761e648d38b1a4e28258306d137fed8ef5be0410
atourianski <a.tourianski@gmail.com>
2016-08-09 18:10:35 -0400

    "file made to experiment with go"

---
+++ golang/work/src/github.com/atourianski/hello/play.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("hello world")
+
+age := 21
+
+randNum := 5
+
+fmt.Println(age - randNum)
+
+myName := "Alena"
+
+fmt.Println("My name is " + myName)
+
+fmt.Printf("%x \nn", 100)
+
+i := 1
+
+for i < 11 {
+
+	fmt.Println(i)
+
+	i++
+}
+
+for j := 12; j > 5; j-- {
+
+	if j == 12{
+		fmt.Println("twelve")
+	}else{
+		fmt.Println(j)}
+}
+
+
+
+}
+
