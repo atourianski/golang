240efb2e4668497a0389394ceab1a3876b78ff3c
atourianski <a.tourianski@gmail.com>
2016-08-11 23:03:16 -0400

    "experimenting"

--- golang/work/src/github.com/atourianski/hello/play.go
+++ golang/work/src/github.com/atourianski/hello/play.go
@@ -34,7 +34,42 @@ for j := 12; j > 5; j-- {
 		fmt.Println(j)}
 }
 
+var favNums[5] float64
+
+favNums[0] = 0.25
+favNums[1] = 1.13
+favNums[2] = 57.9
+favNums[4] = 5.54
+
+fmt.Println(favNums[2])
+
+x := 6; x += 1
+
+fmt.Println(x)
+
+fmt.Println(32132*42452)
+
+place := "first "
+fmt.Println(place)
+
+var (
+
+ a = 2
+ b = 10
+ c = "string"
+ d = false
+)
+
+another := "another variable"
+var yet int
+yet = 44
+fmt.Println(another, yet)
+
+fmt.Println(a, b, c, d)
+
 
 
 }
 
+
+	
