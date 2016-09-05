c7010c30011760469179e47c6fb32e95ebdde95b
atourianski <a.tourianski@gmail.com>
2016-09-05 12:46:16 -0400

    "fixed method"

--- golang/work/src/github.com/atourianski/hello/structs.go
+++ golang/work/src/github.com/atourianski/hello/structs.go
@@ -17,8 +17,9 @@ length, width int
 
 }
 
-func (r1 *rectangle) area() int{	//this is a method, that is, a function that is attached to a given					       // type, it should have a 'receiver', the receiver here is 
-	return r1.length * r1.width	//(r1 *rectangle)
+func (r1 *rectangle) area() int{//this is a method, that is, a function that is attached to a given	
+// type, it should have a 'receiver', the receiver here is 
+	return r1.length * r1.width//(r1 *rectangle)
 
 }
 
