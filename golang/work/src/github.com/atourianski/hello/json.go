0b0539343eae61225677ab22286d9a630b775a2c
atourianski <a.tourianski@gmail.com>
2016-09-13 15:22:57 -0400

    "learning about json"

--- golang/work/src/github.com/atourianski/hello/json.go
+++ golang/work/src/github.com/atourianski/hello/json.go
@@ -5,20 +5,38 @@ import (
 	"fmt"
 )
 
-type box struct {
-	width  int
-	height int
-	color  string
-	open   bool
+type Box struct {
+	Width  int //make sure all the names are uppercase, or they won't be exported
+	Height int
+	Color  string
+	Open   bool
+}
+
+func ExampleMarshal() {
+	LittleBox := Box{Width: 10, Height: 15, Color: "pink", Open: true}
+
+	b, _ := json.Marshal(LittleBox) //json.Marshall takes a go instance and converts it to json data (encodes it)
+
+	s := string(b)
+
+	fmt.Println(s)
+
 }
 
 func main() {
 
-	littleBox := box{width: 10, height: 15, color: "pink", open: true}
+	ExampleMarshal()
 
-	b, _ := json.Marshal(littleBox)
+	//json.Unmarshal takes json data and converts it back to go code (decodes it)
 
-	s := string(b)
+	text := "[{\"Width\":10,\"Height\":15,\"Color\":\"pink\",\"Open\":true}]"
 
-	fmt.Println(s) //output is {} ...why?
+	bytes := []byte(text)
+
+	x := []string{}
+
+	json.Unmarshal(bytes, &x)
+
+	fmt.Println(x)
+
 }
